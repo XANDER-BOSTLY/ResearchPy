3fa0c5ec6e0dac9e032266359a9cccbbfe89f802
Swayambhuba Pattanayak <pattanayak.swayambhuva@gmail.com>
2024-12-17 13:36:40 +0530

    Test printing

--- .venv/1. Using Comments.py
+++ .venv/1. Using Comments.py
@@ -1,1 +1,17 @@
 # jayaguru, I'm starting my coding
+# This is a single line comment
+x = 10 # Assigns the value of 10 to the variable x
+y = 20 # Assigns the value of 20 to the variable y
+
+result = x + y # Calculates 10 + 20
+print("The result is", result)  # Prints "The result is 30"
+
+# PyCharm shortcuts
+#
+# 1. To comment out multiple lines, select the lines and use the following command
+#         Ctrl + /
+#    To uncomment it, use the same command again.
+
+# 2. Press Shift + Alt + . to make font size larger
+#
+# 3. Press Shift + Alt + , to make font size smaller
