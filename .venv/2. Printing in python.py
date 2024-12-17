71f373010c598a606de5126de3206d3375021d1a
Swayambhuba Pattanayak <pattanayak.swayambhuva@gmail.com>
2024-12-17 13:35:35 +0530

    Test printing

---
+++ .venv/2. Printing in python.py
@@ -0,0 +1,24 @@
+print("Hello, world!")
+print("I'm learning Python!")
+print("I'm using double quotation mark!")
+print('This string uses single quotation mark!')
+
+print("First line\nSecond line")
+print("This\tstring\tis\ttabbed")
+
+#age = input("What is your age? ")
+#print("My name is", "Swayam", "and I am", age, "years old")
+
+print('Reetsa once said, "I love ice-creams!!". Mona\'s reaction was priceless.')
+#print("This is indented")
+
+print("Hello" + ", " + "World!")
+print('The backslash symbol,\'\\\' is a special symbol that tels python that the \ncharacter that follows is not meant to be taken in its usual meaning. \n\tIt helps escape the usual meaning!!')
+
+print("""
+Escape characters - 
+\\n - Starts a new line
+\\t - Adds a tab spacing
+\\ - Prints a backslash
+\' - Prints a single quotes
+\" - Prints a double quotes""")
